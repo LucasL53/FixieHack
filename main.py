f4c129a9213ce46cab09f7805535cd20486e0f57
Komori <shin1025@uw.edu>
2023-03-11 13:23:51 -0800

    added requirements

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 
 import fixieai
+import uwtools
 
 BASE_PROMPT = """I'm an agent that rolls virtual dice!"""
 FEW_SHOTS = """
@@ -18,6 +19,27 @@ Q: Roll 3d8
 Ask Func[roll]: 8 3
 Func[roll] says: 5 3 8
 A: You rolled 5, 3, and 8, for a total of 16.
+
+Q: Show the description of CSE133
+Ask Func[roll]: CSE133 Description
+Func[roll] says: Intermediate data programming. Topics include writing programs that manipulate different types of data; leveraging the growing ecosystem of tools and libraries for data programming; writing programs that are both efficient and elegant; and writing medium-scale programs (100 to 200 lines). Prerequisite: either CSE 122, CSE 123, CSE 142, CSE 143, or CSE 160.
+A: Here is the description of CSE133: Intermediate data programming. Topics include writing programs that manipulate different types of data; leveraging the growing ecosystem of tools and libraries for data programming; writing programs that are both efficient and elegant; and writing medium-scale programs (100 to 200 lines). Prerequisite: either CSE 122, CSE 123, CSE 142, CSE 143, or CSE 160.
+
+Q: Give me the description of CSE163
+Ask Func[roll]: CSE163 Description
+Func[roll] says: Intermediate data programming. Topics include writing programs that manipulate different types of data; leveraging the growing ecosystem of tools and libraries for data programming; writing programs that are both efficient and elegant; and writing medium-scale programs (100 to 200 lines). Prerequisite: either CSE 122, CSE 123, CSE 142, CSE 143, or CSE 160.
+A: Here is the description of CSE163: Intermediate data programming. Topics include writing programs that manipulate different types of data; leveraging the growing ecosystem of tools and libraries for data programming; writing programs that are both efficient and elegant; and writing medium-scale programs (100 to 200 lines). Prerequisite: either CSE 122, CSE 123, CSE 142, CSE 143, or CSE 160.
+
+Q: What are the prerequisites of CSE400
+Ask Func[roll]: CSE400 Prerequisites
+Func[roll] says: CSE122,CSE123,CSE142,CSE143,CSE160
+A: Here are the prerequisites of CSE400: CSE122,CSE123,CSE142,CSE143,CSE160
+
+Q: How many credits is LING200?
+Ask Func[roll]: LING200 Credits
+Func[roll] says: 5
+A: LING200 is a 5 credits class.
+
 """
 agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS)
 
@@ -27,3 +49,14 @@ def roll(query: fixieai.Message) -> str:
     dsize, num_dice = query.text.split()
     dice = [random.randint(1, int(dsize)) for _ in range(int(num_dice))]
     return " ".join([str(x) for x in dice])
+
+
+course_catalog_dict = {}
+
+
+@agent.register_func
+def get_class_information(query: fixieai.Message) -> str:
+    global course_catalog_dict
+    class_code, category = query.text.split()
+    course_catalog_dict = uwtools.course_catalogs(struct='dict')
+    return course_catalog_dict[class_code][category]
